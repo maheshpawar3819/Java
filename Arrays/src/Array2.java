76dc43b2090546b8725c8decb3caa64a622e013a
maheshpawar3819 <pawarmahesh3819@gmail.com>
2024-10-18 12:09:23 +0530

    add java files

---
+++ Arrays/src/Array2.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+	
+		//Arrays
+		int arr []=new int[3];
+		//shore values in the array
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		
+		//methods of printing arrays
+//		1.with using for loop
+//		2.with using for each loop
+//		2.with using arrays method
+		
+//		for(int i=0;i<arr.length;i++) {
+//			System.out.println(arr[i]);
+//		}
+		
+//		for(int x:arr) {
+//			System.out.println(x);
+//		}
+		
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
---
+++ Arrays/src/SimpleArray.java
@@ -0,0 +1,21 @@
+
+public class SimpleArray {
+
+	public static void main(String[] args) {
+		
+		int arr []=new int[3];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		
+		System.out.println(arr);
+		
+		char str[]=new char[2];
+		str[0]='a';
+		str[1]='b';
+		
+		System.out.println(str);
+	
+	}
+
+}
