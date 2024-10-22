0de2df2a5d2a76dd4ce5bf4bb1338c9fe73b5ea7
maheshpawar3819 <pawarmahesh3819@gmail.com>
2024-10-22 12:02:03 +0530

    add files

---
+++ src/Simple_Example.java
@@ -0,0 +1,16 @@
+
+public class Simple_Example {
+
+	public static void main(String[] args) {
+		
+		//create a object
+		String name=new String("Mahesh");
+		System.out.println(name);
+		
+		//create direct
+		String name2="Mahesh Pawar";
+		System.out.println(name2);
+
+	}
+
+}
