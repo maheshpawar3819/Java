08be160336bb3e76977eb1edf97870ebeb458244
Mahesh Pawar <150821505+maheshpawar3819@users.noreply.github.com>
2024-11-15 14:06:59 +0530

    add java file contain program of find larger numbers within two numbers

---
+++ FindLargeOfTwoNumbers.java
@@ -0,0 +1,16 @@
+
+public class FindLargeOfTwoNumbers {
+
+	public static void main(String[] args) {
+		//program to find larger of two numbers
+		int a=10;
+		int b=0;
+		if(a>b) {
+			System.out.println("a is greater than b");
+		}else {
+			System.out.println("b is greater than a");
+		}
+
+	}
+
+}
