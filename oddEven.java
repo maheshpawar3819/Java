d3d20e5e76e9d43ff4977a5d60856a511963d41e
Mahesh Pawar <150821505+maheshpawar3819@users.noreply.github.com>
2024-12-21 12:21:03 +0530

    practice of odd even numbers with using for loop

---
+++ oddEven.java
@@ -0,0 +1,23 @@
+
+public class oddEven {
+
+	public static void main(String[] args) {
+		int num=7;
+		
+//		if(num % 2 ==0) {
+//			System.out.println("Number is even");
+//		}else {
+//			System.out.println("Number is odd");
+//		}
+		
+		for(int i=1;i<=10;i++) {
+			if(i % 2 !=0) {
+				
+				
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
